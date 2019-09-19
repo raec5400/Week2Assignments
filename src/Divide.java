800982a57f2ed7f2cdb73d0b71a38a4a558efdff
raec5400 <raec5400@ISS2V1PKH2.scdsb.pri>
2019-09-19 17:33:25 -0400

    Update 9/19/2019

--- src/Divide.java
+++ src/Divide.java
@@ -4,14 +4,22 @@ public class Divide {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int num,num1, num2;
-        for (int i = 0; i < 2; i++) {
-            while(true){
-            System.out.println("Enter number " +(i+1)+ ": ");
-            num=s.nextInt();
-           
+        int num1, num2;   
+        int quo, remain;
+            System.out.print("Enter 1st number: ");
+            num1=s.nextInt();           
+            System.out.print("Enter 2nd number: ");
+            num2=s.nextInt();
+            if(num1>num2){
+                quo = num1/num2;
+                remain = num1 - quo*num2;
             }
-        }
+            else{
+                quo = num2/num1;
+                remain = num2-quo*num1;
+            }
+        System.out.format("Quotient: %d\n", quo);
+        System.out.format("Remainder: %d\n", remain);
         
     }
     

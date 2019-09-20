1bb0e257a279958119443b53c6fc82a243073e76
Rae Chang <Rae Chang@DESKTOP-BB5M2SI>
2019-09-20 19:22:50 -0400

    Update 9/20/2019

--- src/PowersChart.java
+++ src/PowersChart.java
@@ -5,6 +5,7 @@ public class PowersChart {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int exp;
+        int result;
         int x =2;
         while(true){
         System.out.print("Enter exponent (0 to 10): ");
@@ -12,9 +13,15 @@ public class PowersChart {
         if(exp<0 || exp >10) System.out.println("Error - must enter number between 0 to 10.");
         else break;
         }
-        System.out.format("%s%10", "X", "2^X");
-        System.out.format("%s%10", "--", "----");
-        System.out.println(Math.pow(x,exp));
+        
+        System.out.format("%s%10s\n", "X", "2^X");
+            System.out.format("%s%10s\n", "--", "----");  
+            
+        for (int i = 0; i < exp+1; i++) {                      
+            result = (int)(Math.pow(x, i));
+            System.out.format("%2d%10d\n", i, result);
+        }
+        
     }
     
 }

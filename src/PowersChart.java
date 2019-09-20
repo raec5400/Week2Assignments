a7a25a877c9491d76dbeae82c96c525a4197789a
raec5400 <raec5400@ISS2V1PKH2.scdsb.pri>
2019-09-20 12:00:11 -0400

    Update 9/20/2019

--- src/PowersChart.java
+++ src/PowersChart.java
@@ -4,6 +4,17 @@ public class PowersChart {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        int exp;
+        int x =2;
+        while(true){
+        System.out.print("Enter exponent (0 to 10): ");
+        exp = s.nextInt();
+        if(exp<0 || exp >10) System.out.println("Error - must enter number between 0 to 10.");
+        else break;
+        }
+        System.out.format("%s%10", "X", "2^X");
+        System.out.format("%s%10", "--", "----");
+        System.out.println(Math.pow(x,exp));
     }
     
 }

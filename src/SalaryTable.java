3d576e27f8516483351a66ffa95b245a9ebd04b7
Rae Chang <Rae Chang@DESKTOP-BB5M2SI>
2019-09-20 22:14:45 -0400

    Update 9/20/2019

--- src/SalaryTable.java
+++ src/SalaryTable.java
@@ -4,15 +4,15 @@ public class SalaryTable {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        double old=0, raise=40000, newsal=0;
+        double old=40000, raise=0, newsal=0;
         System.out.format("%s%20s%15s%20s\n", "YEAR", "OLD SALARY", "RAISE", "NEW SALARY");
         System.out.format("%s%20s%15s%20s\n", "----", "----------", "-----", "----------");
-        
-        for (int i = 1; i < 11; i++) {           
-            old = newsal;
+        System.out.format("%2s%17s%21s%18s\n", "1", "-", "$40000", "$40000");
+        for (int i = 2; i < 11; i++) {           
             raise = old*0.03;
             newsal = old + raise;
-            System.out.format("%2d%18.2f%18.2f%18.2f\n", i, old, raise, newsal);
+            System.out.format("%2d%21.2f%17.2f%18.2f\n", i, old, raise, newsal);
+            old = newsal;
         }
     }
     
